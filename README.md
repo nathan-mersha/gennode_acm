800cd01f3ed60bb4b64c5363c95b12110de45e47
Nathan <nathanmersha@gmail.com>
2019-08-19 15:30:16 +0300

    Completed first design iteration and added readme file

---
+++ README.md
@@ -0,0 +1,134 @@
+# GenNode ACM
+
+#### Author
+Nathan Mersha
+
+### Installation
+
+GennodeACM library is available on npm, type:
+
+`$ npm i gennode_acm`
+
+
+### Description
+This module is a library to be plugged in your server. This library is one part of a cluster modules as defined in [gennode_authorization](https://www.npmjs.com/package/gennode_authorization)
+The module is used to set access control model data on an object preferable on a post save hook as illustrated in the example below.
+
+### How to use it
+
+###### 1. Initialize the library with a constructor
+
+```javascript
+let gennodeACM = require('gennode_acm');
+let gennodeACMInstance = new gennodeACM({
+    host        : "localhost",
+    port        : 3400,
+    endpoint    : "/auth/token/validate",
+    connection  : "http"
+});
+```
+
+| Configuration | Description | Default |
+|:------------:|:-----------:|:-----------:|
+|host               |Gennode Authorization service host | localhost |
+|port               |Gennode Authorization port         | 3400 |
+|endpoint           |Token validator endpoint           | /auth/token/validate |
+|connection         |Communication type (Current support : http) for future seneca support | http |
+
+###### 2. Define your schema
+
+```javascript
+let schema = new Schema(
+    // Your Schema goes here
+);
+
+```
+
+###### 3. On a post save hook set your acm with default allowed accesses as illustrated below.
+
+```javascript
+schema.post('save', function(doc) {
+    let objectACM = {
+        object : doc._id, // Defines the object id ( or anything you consider unique )
+        accessControl : { // Defines the default access roles on your data
+            read : ["any"],
+            update : ["Admin 1", "Admin 2", "Admin 3"],
+            delete : ["Admin 2"],
+        }
+    };
+
+    // Set acm here.
+    gennodeACMInstance.setACM(objectACM, function (error, response, body) {
+        console.log(response);
+    });
+});
+```
+
+###### Response
+```json
+{
+    "acmSubjects": [
+        {
+            "accessControl": {
+                "read": [],
+                "update": [
+                    "5cee730bb2820d47582e9abd"
+                ],
+                "delete": []
+            },
+            "_id": "5d5a958cd9d8663585e8f342",
+            "subject": "Admin 3",
+            "firstModified": "2019-08-19T12:26:52.223Z",
+            "lastModified": "2019-08-19T12:26:52.223Z",
+            "__v": 0
+        },
+        {
+            "accessControl": {
+                "read": [],
+                "update": [
+                    "5cee730bb2820d47582e9abd"
+                ],
+                "delete": [
+                    "5cee730bb2820d47582e9abd"
+                ]
+            },
+            "_id": "5d5a958cd9d8663585e8f346",
+            "subject": "Admin 2",
+            "firstModified": "2019-08-19T12:26:52.232Z",
+            "lastModified": "2019-08-19T12:26:52.232Z",
+            "__v": 0
+        },
+        {
+            "accessControl": {
+                "read": [],
+                "update": [
+                    "5cee730bb2820d47582e9abd"
+                ],
+                "delete": []
+            },
+            "_id": "5d5a958cd9d8663585e8f345",
+            "subject": "Admin 1",
+            "firstModified": "2019-08-19T12:26:52.232Z",
+            "lastModified": "2019-08-19T12:26:52.232Z",
+            "__v": 0
+        },
+        {
+            "accessControl": {
+                "read": [
+                    "5cee730bb2820d47582e9abd"
+                ],
+                "update": [],
+                "delete": []
+            },
+            "_id": "5d5a958cd9d8663585e8f343",
+            "subject": "any",
+            "firstModified": "2019-08-19T12:26:52.224Z",
+            "lastModified": "2019-08-19T12:26:52.224Z",
+            "__v": 0
+        }
+    ]
+}
+```
+### Contributing
+**If you have anything in mind, feel free to create an issue [here](https://github.com/nathan-mersha/gennode_acm), 
+or fork the project.**
---
+++ index.js
@@ -0,0 +1,117 @@
+/**
+ * @author              - Nathan Mersha
+ * @copyright           - August 2019
+ * @name                - Gennode ACM Middleware
+ * @description         - Library for setting acm data
+ */
+
+class GennodeACM {
+
+    /**
+     * @name                    - Constructor
+     * @description             - Gennode ACM library constructor
+     * @param userConfig        - User configuration
+     * @example
+     * {
+            host            : "localhost",
+            port             : 3400,
+            endpoint         : "/auth/token/validate",
+            connection       : "http" // http call, seneca // todo future seneca support
+       }
+     */
+    constructor(userConfig){
+        // Binding methods
+        this.mergeConfig = this.mergeConfig.bind(this);
+        this.sendRequest = this.sendRequest.bind(this);
+
+        this.debug      = require('debug')('Gennode-ACM-Middleware');
+        this.request    = require('request');
+        this.xtend      = require('xtend');
+        this.exec       = require('child_process').exec;
+
+        // Define default configuration
+        this.defaultConfig = {
+            host    : "localhost",
+            port    : 3400,
+            endpoint : "/auth/acm",
+            connection : "http"
+        };
+
+        // Merge default configuration with the user's configuration.
+        this.mergedConfig = this.mergeConfig(this.defaultConfig,userConfig);
+
+        // ping authorization server and log status
+        this.exec(`ping -c 1 ${this.mergedConfig.host.toString().replace("http://", "").replace("https://", "")}`, (error, stdout, stderr)=> {
+            error || stderr ? this.debug(error, '\n\n', stderr, '\n', `Your authorization server may not be up at ${this.mergedConfig.host}`) : this.debug(stdout);
+        });
+    }
+
+    /**
+     * @name                    - Merge Config
+     * @description             - Merges default config with user config.
+     * @param defaultConfig     - Default Configuration
+     * @param userConfig        - Input user configuration
+     * @returns {*}             - Merged Configuration
+     */
+    mergeConfig(defaultConfig, userConfig){
+        let mergedConfig = this.xtend(defaultConfig, userConfig);
+        // Make message merge config if user has provided config message values.
+        if(userConfig !== undefined){
+            mergedConfig.message = this.xtend(defaultConfig.message, userConfig.message);
+        }
+        return mergedConfig;
+    }
+
+    /**
+     * @name                    - Send request
+     * @description             - Sends request
+     * @param body              - Body to send
+     * @param method            - Http Method
+     * @param endPoint          - Endpoint
+     * @param callback          - Callback function (error, response, body)
+     */
+    sendRequest(body, method, endPoint, callback){
+
+        let options = option(method,body); // defines sending options
+        this.request(endPoint,options,function (err,res,body) {
+            callback(err,res,body)
+        });
+
+        /**
+         * @name                - Option
+         * @description         - Constructs option object
+         * @param method        - Http Method
+         * @param body          - Request body
+         * @returns {{method: *, json: boolean, body: *}}
+         */
+        function option           (method,body)    {
+            return {
+                method : method, // defines the method PUT,GET,DELETE,REMOVE
+                json : (body !== null), // defines true only when body data is available to attach
+                body : body
+            }
+        }
+    }
+
+    /**
+     * @name                    - Set ACM
+     * @description             - Sets acm data
+     * @param objectACM         - Object acm data
+     * @param callback          - Callback function (error, response, body)
+     */
+    setACM(objectACM, callback){
+        let objectACMKeys = Object.keys(objectACM);
+        if(!objectACMKeys.includes("object")){
+            throw new Error("Object acm must contain key object")
+        }else if(!objectACMKeys.includes("accessControl")){
+            throw new Error("Object acm must contain key accessControl");
+        }
+
+        let constructedEndpoint = `http://${this.mergedConfig.host}${this.mergedConfig.port ? `:${this.mergedConfig.port}` : ""}${this.mergedConfig.endpoint}?createBy=object`;
+        this.sendRequest(objectACM, "POST", constructedEndpoint, callback);
+    }
+
+}
+
+
+module.exports = GennodeACM;
